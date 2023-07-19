ba4ca3ca2aef6c21c270b13410b797844323717b
Will Chen <wfchen1990@gmail.com>
2023-07-19 09:13:54 -0600

    Add it1

---
+++ lib/passenger.rb
@@ -0,0 +1,22 @@
+class Passenger
+  attr_reader :name, :age
+
+  def initialize(details)
+    @name = details["name"]
+    @age = details["age"]
+    @drive = false
+  end
+
+  def adult?
+    @age >= 18
+  end
+
+  def driver?
+    @drive
+  end
+
+  def drive
+    @drive = true
+  end
+
+end
---
+++ spec/passenger_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require './lib/passenger'
+
+RSpec.describe Passenger do
+  it 'exists and has readable attributes' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    expect(charlie).to be_a(Passenger)
+    expect(taylor).to be_a(Passenger)
+    expect(charlie.name).to eq("Charlie")
+    expect(charlie.age).to eq(18)
+  end
+
+  it '.adult, .driver?, .drive' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    expect(charlie.adult?).to be true
+    expect(taylor.adult?).to be false
+    expect(charlie.driver?).to be false
+    charlie.drive
+    expect(charlie.driver?).to be true
+  end
+
+end
